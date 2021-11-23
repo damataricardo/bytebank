b72c3d49a8b5d9bf4ff47c92de08bfd0c1dba6d4
Ricardo Rodrigues da Mata Junior <ricardo-mata-junior@hotmail.com>
2021-11-23 20:43:24 -0300

    codigo refatorado

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,50 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
+void main() => runApp(const BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  const BytebankApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      const MaterialApp(
         home: Scaffold(
-          body: ListaTransferencias(),
-          appBar: AppBar(
-            title: Text('Transferências'),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            child: Icon(Icons.add),
-          ),
+          body: FormularioTransferencia(),
         ),
-      ),
-    );
+      );
+  }
+}
+
+
+class FormularioTransferencia extends StatelessWidget {
+  const FormularioTransferencia({Key? key}) : super(key: key);
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
 
 class ListaTransferencias extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-   return  Column(
-     children: <Widget>[
-       ItemTransferencia(Transferencia(100.0, 1000)),
-       ItemTransferencia(Transferencia(200.0, 1001)),
-       ItemTransferencia(Transferencia(300.0, 1002)),
-     ],
+   return  Scaffold(
+     appBar: AppBar(
+       title: Text('Transferências'),
+     ),
+     body: Column(
+       children: <Widget>[
+         ItemTransferencia(Transferencia(100.0, 1000)),
+         ItemTransferencia(Transferencia(200.0, 1001)),
+         ItemTransferencia(Transferencia(300.0, 1002)),
+       ],
+     ),
+     floatingActionButton: FloatingActionButton(
+       onPressed: () {},
+       child: Icon(Icons.add),
+     ),
    );
   }
 }
