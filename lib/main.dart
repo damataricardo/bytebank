46bdf5bfcdc23987f04aa89843d2752835d04cf0
Ricardo Rodrigues da Mata Junior <ricardo-mata-junior@hotmail.com>
2021-11-23 21:26:38 -0300

    implementando functionbutton

--- lib/main.dart
+++ lib/main.dart
@@ -7,69 +7,105 @@ class BytebankApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return
-      const MaterialApp(
-        home: Scaffold(
-          body: FormularioTransferencia(),
-        ),
-      );
+    return  MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
+      ),
+    );
   }
 }
-
 
 class FormularioTransferencia extends StatelessWidget {
-  const FormularioTransferencia({Key? key}) : super(key: key);
- 
+  FormularioTransferencia({Key? key}) : super(key: key);
+
+  final TextEditingController _controladorCampoNumeroConta = TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Criando Transferência'),
+        ),
+        body: Column(
+          children: [
+             Padding(
+              padding: EdgeInsets.all(16.0),
+              child: TextField(
+                style: TextStyle(fontSize: 24.0),
+                decoration: InputDecoration(
+                  labelText: 'Número da conta',
+                  hintText: '0000',
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+             Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: TextEditingController(),
+                style: TextStyle(fontSize: 24.0),
+                decoration: InputDecoration(
+                  icon: Icon(Icons.monetization_on),
+                  labelText: 'Valor',
+                  hintText: '0000',
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            RaisedButton(
+              child: Text('Confirmar'),
+              onPressed: () {
+                debugPrint(_controladorCampoValor.text);
+                debugPrint(_controladorCampoNumeroConta.text);
+              },
+            )
+          ],
+        ));
   }
 }
 
-
-class ListaTransferencias extends StatelessWidget{
+class ListaTransferencias extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   return  Scaffold(
-     appBar: AppBar(
-       title: Text('Transferências'),
-     ),
-     body: Column(
-       children: <Widget>[
-         ItemTransferencia(Transferencia(100.0, 1000)),
-         ItemTransferencia(Transferencia(200.0, 1001)),
-         ItemTransferencia(Transferencia(300.0, 1002)),
-       ],
-     ),
-     floatingActionButton: FloatingActionButton(
-       onPressed: () {},
-       child: Icon(Icons.add),
-     ),
-   );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
+      ),
+      body: Column(
+        children: <Widget>[
+          ItemTransferencia(Transferencia(100.0, 1000)),
+          ItemTransferencia(Transferencia(200.0, 1001)),
+          ItemTransferencia(Transferencia(300.0, 1002)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
 
 class ItemTransferencia extends StatelessWidget {
-
   final Transferencia _transferencia;
-
 
   ItemTransferencia(this._transferencia);
 
   @override
   Widget build(BuildContext context) {
-    return  Card(
+    return Card(
         child: ListTile(
-          leading: Icon(Icons.monetization_on),
-          title: Text(_transferencia.valor.toString()),
-          subtitle: Text(_transferencia.numeroConta.toString()),
-        )
-    ) ;
+      leading: Icon(Icons.monetization_on),
+      title: Text(_transferencia.valor.toString()),
+      subtitle: Text(_transferencia.numeroConta.toString()),
+    ));
   }
-
 }
 
-class Transferencia{
+class Transferencia {
   final double valor;
   final int numeroConta;
 
