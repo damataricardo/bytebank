27f701d06bcddaab780c0c4d5c3aa4e1f254b17c
Ricardo Rodrigues da Mata Junior <ricardo-mata-junior@hotmail.com>
2021-11-24 17:43:07 -0300

    refatorando codigo

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(const BytebankApp());
+void main() => runApp(BytebankApp());
 
 class BytebankApp extends StatelessWidget {
-  const BytebankApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: FormularioTransferencia(),
       ),
@@ -16,12 +16,9 @@ class BytebankApp extends StatelessWidget {
 }
 
 class FormularioTransferencia extends StatelessWidget {
-  FormularioTransferencia({Key? key}) : super(key: key);
-
-  final TextEditingController _controladorCampoNumeroConta = TextEditingController();
+  final TextEditingController _controladorCampoNumeroConta =
+      TextEditingController();
   final TextEditingController _controladorCampoValor = TextEditingController();
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -30,40 +27,59 @@ class FormularioTransferencia extends StatelessWidget {
           title: Text('Criando Transferência'),
         ),
         body: Column(
-          children: [
-             Padding(
-              padding: EdgeInsets.all(16.0),
-              child: TextField(
-                style: TextStyle(fontSize: 24.0),
-                decoration: InputDecoration(
-                  labelText: 'Número da conta',
-                  hintText: '0000',
-                ),
-                keyboardType: TextInputType.number,
-              ),
-            ),
-             Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                controller: TextEditingController(),
-                style: TextStyle(fontSize: 24.0),
-                decoration: InputDecoration(
-                  icon: Icon(Icons.monetization_on),
-                  labelText: 'Valor',
-                  hintText: '0000',
-                ),
-                keyboardType: TextInputType.number,
-              ),
+          children: <Widget>[
+            Editor(
+                controlador: _controladorCampoNumeroConta,
+                dica: '000',
+                rotulo: 'Número da conta'),
+            Editor(
+              dica: '0.00',
+              controlador: _controladorCampoValor,
+              rotulo: 'valor',
+              icone: Icons.monetization_on,
             ),
             RaisedButton(
               child: Text('Confirmar'),
-              onPressed: () {
-                debugPrint(_controladorCampoValor.text);
-                debugPrint(_controladorCampoNumeroConta.text);
-              },
+              onPressed: () => _criaTransferencia(),
             )
           ],
         ));
+  }
+
+  void _criaTransferencia() {
+     final int? numeroConta =
+        int.tryParse(_controladorCampoNumeroConta.text);
+    final double? valor =
+        double.tryParse(_controladorCampoValor.text);
+    if (numeroConta != null && valor != null) {
+      final transferenciaCriada = Transferencia(valor, numeroConta);
+    }
+  }
+}
+
+class Editor extends StatelessWidget {
+  final TextEditingController? controlador;
+  final String? rotulo;
+  final String? dica;
+  final IconData? icone;
+
+  Editor({this.controlador, this.rotulo, this.dica, this.icone});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        controller: controlador,
+        style: TextStyle(fontSize: 24.0),
+        decoration: InputDecoration(
+          icon: icone != null ? Icon(icone) : null,
+          labelText: rotulo,
+          hintText: dica,
+        ),
+        keyboardType: TextInputType.number,
+      ),
+    );
   }
 }
 
@@ -110,4 +126,9 @@ class Transferencia {
   final int numeroConta;
 
   Transferencia(this.valor, this.numeroConta);
+
+  @override
+  String toString() {
+    return 'Transferencia{valor: $valor, numeroConta: $numeroConta}';
+  }
 }
